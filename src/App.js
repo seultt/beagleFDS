aa30ab998e2a123cf0d73bbbf56c199f5b00220b
seultt <seultt@gmail.com>
2017-10-12 21:13:21 +0900

    프로젝트 세팅

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './scss/style.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="colors">
+        colors
+      </div>
+    );
+  }
+}
+
+export default App;
