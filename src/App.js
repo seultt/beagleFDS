705a9bc6f7b80186cd1b5bbb8dd7ee9b786f509a
seultt <seultt@gmail.com>
2017-10-14 00:49:07 +0900

    설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './scss/main.scss';
+import Chat from './pages/Chat'
+import './scss/main.scss'
 import Header from './components/header/header';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div>
         <Header />
+        <Chat />
       </div>
     );
   }
---
+++ src/pages/Chat.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import message from '../images/icon_message.svg';
+import profile from '../images/icon_profile.svg';
+import arrow from '../images/icon_arrow_down.svg';
+import search from '../images/icon_search.svg';
+import photo from '../images/icon_photo.svg';
+import like from '../images/icon_like.svg';
+import point from '../images/icon_point.svg';
+import calendar from '../images/icon_calendar.svg';
+
+export default class Chat extends Component {
+  render() {
+    return (
+      <main>
+        <div className="__contain">
+          <section className="chatting">
+            가
+          </section>
+          <section className="info">
+            나
+          </section>
+        </div>
+      </main>
+    );
+  }
+}
