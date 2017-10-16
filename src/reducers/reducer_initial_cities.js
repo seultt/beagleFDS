034f835b0e2920be10a0dd8045874a7c84dd7665
yujin-min <yujinmin91@gmail.com>
2017-10-16 14:32:45 +0900

    initial_cities 추가

---
+++ src/reducers/reducer_initial_cities.js
@@ -0,0 +1,24 @@
+export default function() {
+  return [
+    {id: 1, city_name: 'Kyoto'}, 
+    {id: 2, city_name: 'Jeju Island'}, 
+    {id: 3, city_name: 'Paris'}, 
+    {id: 4, city_name: 'Bangkok'}, 
+    {id: 5, city_name: 'London'}, 
+    {id: 6, city_name: 'Tokyo'}, 
+    {id: 7, city_name: 'Rome'}, 
+    {id: 8, city_name: 'Osaka'}, 
+    {id: 9, city_name: 'Fukuoka'}, 
+    {id: 10, city_name: 'Prague'}, 
+    {id: 11, city_name: 'Barcelona'}, 
+    {id: 12, city_name: 'Seoul'}, 
+    {id: 13, city_name: 'New York City'}, 
+    {id: 14, city_name: 'Hong Kong'}, 
+    {id: 15, city_name: 'Los Angeles'}, 
+    {id: 16, city_name: 'Da Nang'}, 
+    {id: 17, city_name: 'Cebu'}, 
+    {id: 18, city_name: 'Hanoi'}, 
+    {id: 19, city_name: 'Sapporo'}, 
+    {id: 20, city_name: 'Singapore'}
+  ]
+}
