b6c65d34c1d09005d714584c586244e5dfacb189
youngseung <gigantes88@gmail.com>
2017-10-18 22:00:57 +0900

    header.js 로그인 부분 onClick 이벤트 문제 해결 - modalLogin.js a태그 -> button 태그로 수정

--- src/components/header/loginModal/modalLogin.js
+++ src/components/header/loginModal/modalLogin.js
@@ -13,39 +13,47 @@ export default class ModalLogin extends React.Component {
         className="join"
         overlayClassName="join__overlay"
       > 
-      <div className="join__container">
-        <a 
-          className="join__facebook--btn"
-          onClick={this.props.loginWithFacebook}
-        >
-          <img src={facebook} />
-          페이스북으로 계속하기
-        </a>
-        <a 
-          className="join__google--btn"
-          onClick={this.props.loginWithGoogle}
-        >
-          <img src={google} />
-          구글로 계속하기
-        </a>
-        <a 
-          className="join__naver--btn"
-          onClick={this.props.loginWithNaver}
-        >
-          네이버로 계속하기
-        </a>
-        <a 
-          className="join__kakao--btn"
-          onClick={this.props.loginWithKakao}
-        >
-          카카오로 계속하기
-        </a>
-        <p>
-        회원 가입 또는 계속하기를 클릭하면 비글의 <span>서비스 약관, 개인정보 보호정책</span>에 동의하는 것 입니다.
-        </p>
-      </div>
-      <button className="join--closeBtn" onClick={this.props.handleModalCloseLogin}>닫기</button>
-    </ReactModal>
+        <div className="join__container">
+          <button 
+            type="button"
+            className="join__facebook--btn"
+            onClick={this.props.login}
+            value="facebook"
+          >
+            <img src={facebook} />
+            페이스북으로 계속하기
+          </button>
+          <button 
+            type="button"
+            className="join__google--btn"
+            onClick={this.props.login}
+            value="google"
+          >
+            <img src={google} />
+            구글로 계속하기
+          </button>
+          <button 
+            type="button"
+            className="join__naver--btn"
+            onClick={this.props.login}
+            value="naver"
+          >
+            네이버로 계속하기
+          </button>
+          <button 
+            type="button"
+            className="join__kakao--btn"
+            onClick={this.props.login}
+            value="kakao"
+          >
+            카카오로 계속하기
+          </button>
+          <p>
+          회원 가입 또는 계속하기를 클릭하면 비글의 <span>서비스 약관, 개인정보 보호정책</span>에 동의하는 것 입니다.
+          </p>
+        </div>
+        <button type="button" className="join--closeBtn" onClick={this.props.handleModalCloseLogin}>닫기</button>
+      </ReactModal>
     )
   }
 }
