cacd97a1f6a0a2db0440f7b468a702b6876b30f8
youngseung <gigantes88@gmail.com>
2017-10-18 20:21:02 +0900

    Filter 대화방리스트 수정 - reducer_chatList_data.js 데이터 수정 Boracay -> Rome - reducer_initial_sort.js 에서 value 값 수정

--- src/action/action_getChatList.js
+++ src/action/action_getChatList.js
@@ -26,3 +26,30 @@ export const getChatList = () => {
     )
   }
 }
+
+// export const getChatList = (param = '?citiy=3&date=201;') => {
+//   return (dispatch) => {
+//     console.log('getChatList 데이터 요청-로딩부분');
+//     dispatch({
+//       type: 'GET_CHAT_LIST_REQUEST',
+//     })
+
+//     axios.get(`https://test.younghea.com/${param}`)
+//     .then(
+//       (res) => {
+//         console.log(res);
+//         dispatch({
+//           type: 'GET_CHAT_LIST_SUCCESS',
+//           payload: res,
+//         })
+//       }
+//     ).catch(
+//       (error) => {
+//         console.error(error)
+//         dispatch({
+//           type: 'GET_CHAT_LIST_FAILED',
+//         }) 
+//       }
+//     )
+//   }
+// }
