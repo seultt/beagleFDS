1a4eb02f24e01073f552403d178268bae2b18e2a
youngseung <gigantes88@gmail.com>
2017-10-23 10:49:37 +0900

    인덱스 리스트 그리기 테스트

--- src/action/action_getChatList.js
+++ src/action/action_getChatList.js
@@ -1,29 +1,34 @@
 import axios from 'axios';
+import SERVER_ADDRESS from '../config'
 
 export const getChatList = () => {
   return (dispatch) => {
-    console.log('getChatList 데이터 요청-로딩부분');
-    dispatch({
-      type: 'GET_CHAT_LIST_REQUEST',
-    })
+    // console.log('getChatList 데이터 요청-로딩부분');
+    // dispatch({
+    //   type: 'GET_CHAT_LIST_REQUEST',
+    // })
 
-    // axios.get(`https://test.younghea.com/`)
-    // .then(
-    //   (res) => {
-    //     // console.log(res);
-    //     dispatch({
-    //       type: 'GET_CHAT_LIST_SUCCESS',
-    //       payload: res,
-    //     })
-    //   }
-    // ).catch(
-    //   (error) => {
-    //     console.error(error)
-    //     dispatch({
-    //       type: 'GET_CHAT_LIST_FAILED',
-    //     }) 
-    //   }
-    // )
+    axios.get(`${SERVER_ADDRESS}/api/chat-list`, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      }
+    })
+    .then(
+      (res) => {
+        console.log('통신은 된겁니까?');
+        dispatch({
+          type: 'GET_CHAT_LIST_SUCCESS',
+          payload: res,
+        })
+      }
+    ).catch(
+      (error) => {
+        console.error(error)
+        // dispatch({
+        //   type: 'GET_CHAT_LIST_FAILED',
+        // }) 
+      }
+    )
   }
 }
 
--- src/reducers/reducer_database_active.js
+++ src/reducers/reducer_database_active.js
@@ -1,31 +1,25 @@
 const INITIAL_STATE = {
-  isLoading: false,
-  errorState: false,
   chatList: [],
 }
 
 const databaseReducer = (state = INITIAL_STATE, action = null) => {
 
-  if (action.type === 'GET_CHAT_LIST_REQUEST')  {
-   return {
-     ...state,
-     isLoading: true,
-   } 
-  }
+  // if (action.type === 'GET_CHAT_LIST_REQUEST')  {
+  //  return {
+  //    ...state,
+  //  } 
+  // }
   if (action.type === 'GET_CHAT_LIST_SUCCESS') {
     return {
       ...state,
-      isLoading: false,
-      chatList: action.payload,
+      chatList: action.payload
     }
   }
-  if (action.type === 'GET_CHAT_LIST_FAILED') {
-    return {
-      ...state,
-      isLoading: false,
-      errorState: true,
-    }
-  }
+  // if (action.type === 'GET_CHAT_LIST_FAILED') {
+  //   return {
+  //     ...state,
+  //   }
+  // }
 
   return {
     ...state,
