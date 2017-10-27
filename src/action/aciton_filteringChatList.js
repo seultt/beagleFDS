8832bad36acf9db9f89532a3943cc464197fcb44
youngseung <gigantes88@gmail.com>
2017-10-27 16:31:23 +0900

    필터링 액션 및 리듀서 추가 - Filter.js에 클릭 이벤트와 스크롤 내릴 때 이벤트의 액션을 다른 파일로 처리 - filter 관련 액션파일 추가

--- src/action/aciton_filteringChatList.js
+++ src/action/aciton_filteringChatList.js
@@ -1,16 +1,15 @@
 import axios from 'axios';
 import SERVER_ADDRESS from '../config'
 
-export const getChatList = (LAST_ID, LAST_LIKE) => {
+export const getFilteringChatList = (FILTER) => {
   return (dispatch) => {
-    // getChatList 데이터 요청-로딩부분
     dispatch({
-      type: 'GET_CHAT_LIST_REQUEST',
+      type: 'GET_FILTERING_CHAT_LIST_REQUEST',
     })
     
     // 데이터 가져오는 부분
     axios.get(
-      `${SERVER_ADDRESS}/api/chat-list?lastId=${LAST_ID}&lastLike=${LAST_LIKE}`
+      `${SERVER_ADDRESS}/api/chat-list?${FILTER}`
       , {
       // CORS 문제 해결하려면 아래 header를 넣어줘야한다.
       headers: {
@@ -20,7 +19,7 @@ export const getChatList = (LAST_ID, LAST_LIKE) => {
       .then(
         (res) => {
           dispatch({
-            type: 'GET_CHAT_LIST_SUCCESS',
+            type: 'GET_FILTERING_CHAT_LIST_SUCCESS',
             payload: res.data,
           })
         }
@@ -28,7 +27,7 @@ export const getChatList = (LAST_ID, LAST_LIKE) => {
       .catch(
         (error) => {
           dispatch({
-            type: 'GET_CHAT_LIST_FAILED',
+            type: 'GET_FILTERING_CHAT_LIST_FAILED',
             payload: console.log(error),
           }) 
         }
