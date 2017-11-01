dab46a1f877d05868fa2b1ffc61e02c41d575c1d
youngseung <gigantes88@gmail.com>
2017-11-01 15:58:09 +0900

    필터에서 무한스크롤할 때 마지막 채팅방의 like 부분 쿼리스트링 추가

--- src/action/action_getChatList.js
+++ src/action/action_getChatList.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import SERVER_ADDRESS from '../config'
 
-export const getChatList = (LAST_ID, condition) => {
+export const getChatList = (LAST_ID, LAST_LIKE, condition) => {
   return (dispatch) => {
     // getChatList 데이터 요청-로딩부분
     dispatch({
@@ -10,7 +10,7 @@ export const getChatList = (LAST_ID, condition) => {
     
     // 데이터 가져오는 부분
     axios.get(
-      `${SERVER_ADDRESS}/api/chat-rooms?lastId=${LAST_ID}&${condition}`
+      `${SERVER_ADDRESS}/api/chat-rooms?lastId=${LAST_ID}&${LAST_LIKE}&${condition}`
       , {
       // CORS 문제 해결하려면 아래 header를 넣어줘야한다.
       headers: {
